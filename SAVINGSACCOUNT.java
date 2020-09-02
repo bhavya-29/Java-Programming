a3472463f6e3d0b59be1f40e5ce958a4933d8501
bhavya-29 <70514092+bhavya-29@users.noreply.github.com>
2020-09-02 13:50:43 +0530

    Add files via upload

---
+++ SAVINGSACCOUNT.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.Scanner;
+class SavingsAccount{
+    static double annualInterestsRate = 0.04;
+    private double savingsBalance;
+    
+    public SavingsAccount(){}
+    
+    public SavingsAccount(double savingsBalance){
+        this.savingsBalance = savingsBalance;
+    }
+    
+    public void setBalance(double savingsBalance){
+        this.savingsBalance=savingsBalance;
+    }
+    
+    public double getBalance(){
+        return(savingsBalance);
+    }
+    
+    
+}
+public class SAVINGSACCOUNT{
+    public static void main(String[] args){
+        SavingsAccount saver1 = new SavingsAccount(2000.00);
+        SavingsAccount saver2 = new SavingsAccount(3000.00);
+        double newBalance1 = saver1.getBalance()+calculateMonthlyInterest(saver1.getBalance(),saver1.annualInterestsRate);
+        double newBalance2 =saver2.getBalance()+calculateMonthlyInterest(saver2.getBalance(),saver2.annualInterestsRate);
+        System.out.println("New Balance of saver1 : "+newBalance1);
+        System.out.println("New Balance of saver2 : "+newBalance2);
+        saver1.setBalance(newBalance1);
+        saver2.setBalance(newBalance2);
+        modifyInterestRate(saver1.annualInterestsRate);
+        modifyInterestRate(saver2.annualInterestsRate);
+        newBalance1 =saver1.getBalance()+calculateMonthlyInterest(saver1.getBalance(),saver1.annualInterestsRate);
+        newBalance2 =saver2.getBalance()+calculateMonthlyInterest(saver2.getBalance(),saver2.annualInterestsRate);
+        System.out.println("New Balance of saver1 : "+newBalance1);
+        System.out.println("New Balance of saver2 : "+newBalance2);
+
+    }
+    public static double calculateMonthlyInterest(double sav_bal,double interest_rate){
+        double monthly_interest=0.0;
+        monthly_interest = (sav_bal*interest_rate)/12;
+        return(monthly_interest);
+    }
+    
+    public static double modifyInterestRate(double interest_rate){
+        interest_rate=0.05;
+        return(interest_rate);
+    }
+}
+
